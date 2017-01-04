728f3d0f06fce479496cb095ce844f497ce40efc
Girai Kaku <kakugirai@gmail.com>
2017-01-05 01:12:42 +0800

    modified zsh_setup.sh

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -1,14 +1,5 @@
-#/bin/bash
-if command -v git >/dev/null 2>&1 ; then
-    echo "Cloning my config files..."
-    git clone https://github.com/kakugirai/rc_file.git
-else
-    echo "git not found"
-fi
-
-echo "Installing Oh My Zsh..."
+sudo apt-get install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp rc_file/my_ys.zsh-theme ~/.oh-my-zsh/themes/
+mkdir ~/.oh-my-zsh/custom/themes
+cp ~/dotfiles/my_ys.zsh-theme ~/.oh-my-zsh/custom/themes/
 sed -i -e 's/ZSH_THEME="robbyrussell"/ZSH_THEME="my_ys"/g' ~/.zshrc
-echo "Done!"
-
